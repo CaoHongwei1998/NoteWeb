4599bae840c4352dad6cd8c9930782a5411f0ca3
CaoHongwei1998 <caohongwei1998@gmail.com>
2020-12-16 10:02:41 +0800

    前端

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+
+// 导入全局样式
+import './assets/css/global.css'
+
+import axios from 'axios'
+import store from './store'
+// 请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8080/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
